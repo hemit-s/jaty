c49c9d2cbc4849e70f24b217154ad63f7b816e86
Darren Li <d338li@uwaterloo.ca>
2021-09-21 22:37:41 -0400

    rearrange some folders and move readme

--- src/coopstop/coopstop/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-import datetime
-
-def get_address_information(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
