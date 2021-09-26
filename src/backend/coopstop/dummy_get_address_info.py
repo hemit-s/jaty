25ad7b2fa77ac75de0df97b421e1fa8d30692af8
li-darren <darren.li@uwaterloo.ca>
2021-09-25 22:02:14 -0400

    add return result expected json

--- src/backend/coopstop/dummy_get_address_info.py
+++ src/backend/coopstop/dummy_get_address_info.py
@@ -8,8 +8,58 @@ payload = {
         "address4",
         "address5",
     ],
-    "destination": "destination",
-    "expected_work_arrival_time": 5000
+    "work_address ": "work_address",
+    # assuming arrive at 9
+    # assume leave at 5
+    # "expected_work_arrival_time": 5000
+}
+
+return_val = {
+
+    # "work_travel_distance": "50km",
+    "travel_times_to_work": {
+        "walking" : "40m",
+        "biking" : "55m",
+        "public_transit" : "35m",
+        "car": "10m"
+    },
+
+    "grocery_locations": [
+        {
+            "name": "Loblaws",
+            "distance" : "4km",
+            # "travel_time" : "4m"
+        },
+        {
+            "name": "Food Basics",
+            "distance" : "5km",
+            # "travel_time" : "4m"
+        },
+        {
+            "name": "Walmart",
+            "distance" : "6km",
+            # "travel_time" : "4m"
+        }
+    ],
+
+    "gym_locations":[
+        {
+            "name": "Goodlife",
+            "distance" : "4km",
+            # "travel_time" : "4m"
+        },
+        {
+            "name": "Lyft Tingz",
+            "distance" : "5km",
+            # "travel_time" : "4m"
+        },
+        {
+            "name": "Git Big",
+            "distance" : "6km",
+            # "travel_time" : "4m"
+        }
+    ]
+
 }
 
 requests.get("http://127.0.0.1:8000/get_address_info", json=payload)
